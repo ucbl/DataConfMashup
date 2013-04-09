0322071cab064a838a08fb340a21ff777a14a023
florianbacle <florian.bacle@etu.univ-lyon1.fr>
2013-04-09 22:34:25 +0200

    Start configurations handling

---
+++ js/core/Query.js
@@ -0,0 +1,21 @@
+  /**   
+ *   Copyright <c> Claude Bernard - University Lyon 1 -  2013
+ *   Author: Lionnel MEDINI(supervisor), Florian BACLE, Fiona LEPEUTREC, Benoît DURANT-DE-LA-PASTELLIERE, NGUYEN Hoang Duy Tan
+ *   Description: Interface of a Datasource model
+ *   Version: 1
+ *   Tags:  
+ **/
+Query = Backbone.Model.extend({
+
+	defaults: {
+		
+	},
+	
+	initialize: function(options){
+		this.
+	}
+	
+	setQuery: function(options){
+		
+	}
+});
